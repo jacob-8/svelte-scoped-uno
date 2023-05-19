fcfaa094f8c9002863a345261a1c4dc2edf6a562
jacob-8 <jwrunner7@gmail.com>
2023-05-20 07:34:15 +0800

    remove bug notice

--- examples/svelte-vite-plugin/src/lib/Prose.svelte
+++ examples/svelte-vite-plugin/src/lib/Prose.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="text-base prose prose-purple md:prose-orange  dark:prose-invert max-w-full text-left mt-10 p-3 md:p-5 shadow-lg border rounded"
+  class="text-base prose prose-purple md:prose-orange dark:prose-invert max-w-full text-left mt-10 p-3 md:p-5 shadow-xl border rounded"
 >
   {@html marked(`# Svelte Scoped Uno with Vite + Svelte
   This project has the following **example usages**:
